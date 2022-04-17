04d054992b63df7a589e275a70d850d012a94a14
sathit.pkn <sathit.pkn@gmail.com>
2022-04-17 17:10:32 +0700

    -

--- service/database.go
+++ service/database.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	//	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -10,11 +9,9 @@ var dbConn *gorm.DB
 
 func CreateConnect() {
 	//	dsn := "root:@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
-
 	//heroku
-	dsn := "host=ec2-34-192-210-139.compute-1.amazonaws.com user=vppzvlmbbbwiqc password=45a8e987c8e6dcc38dcd31ba8bd9a56a6f4894513d164f068cab43d071829826 dbname=d2t1tmuqj0iqk2 port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	dsn := "host=ec2-52-21-136-176.compute-1.amazonaws.com user=wwrxguwgoqqwbg password=54c7d62d01812dc04c0d0f25ba9ced413f1884214dbae2204b0620c72f1bd47c dbname=d7g6h8fm7bfvdt port=5432 sslmode=require TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	//	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
